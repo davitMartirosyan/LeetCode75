7a19fff2f7c31f7d18b8ac7c231406bbaf29a0d3
davitmartirosyan <phpencoding@gmail.com>
2024-12-18 08:57:00 +0400

    Leetcode75 Day11

--- compress.cpp
+++ compress.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "trie.hpp"
 
 int compress(std::vector<char>& chars)
 {
-    
+    std::string s;
+    for(size_t i = 0; i < chars.size();)
+    {
+        for(size_t j = i; j < chars.size();)
+        {
+            if ( chars[i] == chars[j])
+            {
+                j++;
+                if (j == chars.size())
+                {
+                    s+= chars[i];
+                    if ((j - i) > 1)
+                        s += std::to_string(j-i);
+                    i = j;
+                    break;
+                }
+            }
+            else
+            {
+                s += chars[i];
+                if ((j - i) > 1)
+                    s += std::to_string(j-i);
+                i = j;
+                break;
+            }
+        }
+    }
+    for (size_t i = 0; i < s.size(); i++)
+        chars[i] = s[i];
+    chars.erase(chars.begin() + s.size(), chars.end());
+    return (s.size());
 }
